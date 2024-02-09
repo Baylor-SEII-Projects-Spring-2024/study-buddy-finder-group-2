c18b6ce192d3e25cfb2d5219bd22257d9f2c9865
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2024-02-08 21:44:59 -0600

    add testing script

--- qa/get_checks.py
+++ qa/get_checks.py
@@ -1,23 +1,16 @@
 import requests
 
-def get_http_status_codes(urls):
-    responses = []
-    for url in urls:
-        try:
-            response = requests.get(url)
-            responses.append(response.status_code)
-        except requests.RequestException as e:
-            responses.append(str(e))
-    return responses
+responses = []
+urls = ['http://34.16.169.60:3000', 'http://34.16.169.60:8080/ping']
+for url in urls:
+    try:
+        response = requests.get(url)
+        responses.append(response.status_code)
+    except requests.RequestException as e:
+        responses.append(str(e))
 
-def main():
-    urls = ['http://34.16.169.60:3000', 'http://34.16.169.60:8080/ping']
-    status_codes = get_http_status_codes(urls)
-    for code in status_codes:
-        if 200 <= code < 300:
-            print('SUCCESS')
-        else:
-            print('FAILURE: ' + code)
-
-if __name__ == "__main__":
-    main()
+for code in responses:
+    if 200 <= code < 300:
+        print('SUCCESS')
+    else:
+        print('FAILURE: ' + code)
--- qa/health_check.py
+++ qa/health_check.py
@@ -1,16 +1,8 @@
 import subprocess
 
-def ping_server(ip):
-    response = subprocess.run(['ping', '-c', '4', ip], stdout=subprocess.PIPE, text=True)
-    return response.stdout
-
-def main():
-    server_ip = "34.16.169.60"
-    ping_result = ping_server(server_ip)
-    if "0% packet loss" in ping_result:
-        print("Server is up")
-    else:
-        print("Check server")
-
-if __name__ == "__main__":
-    main()
+ip = "34.16.169.60"
+response = subprocess.run(['ping', '-c', '4', ip], stdout=subprocess.PIPE, text=True).stdout
+if "0% packet loss" in response:
+    print("SUCCESS")
+else:
+    print("FAILURE: Check server")
