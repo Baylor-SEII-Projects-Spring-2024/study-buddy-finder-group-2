bd3d3b01dcffa68b4533a5161a412dc6ee46247f
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2024-02-12 21:30:01 -0600

    add page skeletons for pages in draft

---
+++ study-buddy-frontend/src/pages/editMeetup.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <div>
+      <h1>This is the edit meetup page</h1>
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ study-buddy-frontend/src/pages/passwordReset.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <div>
+      <h1>This is the password reset page</h1>
+    </div>
+  );
+}
+
+export default LoginPage;
