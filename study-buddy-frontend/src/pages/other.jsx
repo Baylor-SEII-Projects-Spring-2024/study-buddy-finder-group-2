091132e0e0a251091eb21ea5495c62c49d7e7950
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2024-02-13 01:25:35 -0600

    add others page back

---
+++ study-buddy-frontend/src/pages/other.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OthersInfoPage() {
+  return (
+    <div>
+      <h1>This is the non-logged in user page</h1>
+    </div>
+  );
+}
+
+export default OthersInfoPage;
