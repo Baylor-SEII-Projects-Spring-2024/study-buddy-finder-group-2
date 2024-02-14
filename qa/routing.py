16316fac7c4c1bd916fe8c5f50dbacb81db1fd42
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2024-02-13 20:33:07 -0600

    make routing test more robust, add more pages to test

--- qa/routing.py
+++ qa/routing.py
@@ -9,33 +9,71 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 def test_register_button():
     driver.get('http://34.16.169.60:3000')
 
-    register_button = driver.find_elements(
-                     By.XPATH, '//*[@id="__next"]/main/div/div[2]/a[3]/button')
+    register_button = driver.find_elements(By.XPATH, "//button[contains(text(), 'Register')]")
 
     driver.execute_script("arguments[0].click();", register_button[0])
 
-    time.sleep(2)
+    time.sleep(1)
 
     url = driver.current_url
 
     if url.endswith('/registration'):
         print('SUCCESS')
+    else:
+        print('FAILURE')
 
 def test_login_button():
     driver.get('http://34.16.169.60:3000')
 
-    login_button = driver.find_elements(
-                   By.XPATH, '//*[@id="__next"]/main/div/div[2]/a[1]/button')
+    login_button = driver.find_elements(By.XPATH, "//button[contains(text(), 'Login')]")
 
     driver.execute_script("arguments[0].click();", login_button[0])
 
-    time.sleep(2)
+    time.sleep(1)
 
     url = driver.current_url
 
     if url.endswith('/login'):
         print('SUCCESS')
+    else:
+        print('FAILURE')
+
+def test_invitations_button():
+    driver.get('http://34.16.169.60:3000')
+
+    invitations_button = driver.find_elements(By.XPATH, "//button[contains(text(), 'Invitations')]")
+
+    driver.execute_script("arguments[0].click();", invitations_button[0])
+
+    time.sleep(1)
+
+    url = driver.current_url
+
+    if url.endswith('/invitations'):
+        print('SUCCESS')
+    else:
+        print('FAILURE')
+
+def test_meetups_button():
+    driver.get('http://34.16.169.60:3000')
+
+    meetups_button = driver.find_elements(By.XPATH, "//button[contains(text(), 'View meetups')]")
+
+    driver.execute_script("arguments[0].click();", meetups_button[0])
+
+    time.sleep(1)
+
+    url = driver.current_url
+
+    if url.endswith('/viewMeetups'):
+        print('SUCCESS')
+    else:
+        print('FAILURE')
+
 
 test_register_button()
 test_login_button()
+test_meetups_button()
+test_invitations_button()
+
 driver.quit()
