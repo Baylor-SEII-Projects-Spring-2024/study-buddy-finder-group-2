2800184efa346cf20281acced4fa75a3d234f472
DanielleBoswell <danituongvy@gmail.com>
2024-02-18 15:15:03 -0600

    refined registration fields

--- study-buddy-frontend/src/pages/registration.jsx
+++ study-buddy-frontend/src/pages/registration.jsx
@@ -1,32 +1,47 @@
 import React from 'react';
 import {Button, RadioGroup, TextField, Box, Grid} from '@mui/material/';
-import {FormControlLabel, FormLabel, Radio} from "@mui/material";
+import {ButtonGroup, FormControlLabel, FormLabel, Radio, Typography} from "@mui/material";
 
 function RegistrationPage() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            firstName: data.get('fname'),
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
   return (
-      <div>
-          <h1>This is the registration page</h1>
-          <Box component="form">
+      <Box>
+          <Box component="form" noValidate onSubmit={handleSubmit}
+               sx={{
+                   marginTop: 5,
+                   display: 'flex',
+                   flexDirection: 'column',
+                   alignItems: 'center',
+          }}>
+              <Typography component="h1" variant="h5">Register</Typography><br/>
               <TextField autoComplete="given-name" id="fname" name="fname" label="First Name"/><br/>
               <TextField autoComplete="last-name" id="lname" name="lname" label="Last Name"/><br/>
               <TextField autoComplete="email" id="email" name="email" label="Email"/><br/>
               <TextField id="username" name="username" label="Username"/><br/>
-              <TextField autoComplete="new-password" id="password" name="password" label="Password"/><br/>
+              <TextField autoComplete="new-password" type="password" id="password" name="password" label="Password"/><br/>
               <TextField autoComplete="confirm-password" id="confirm_password" name="confirm_password"
-                         label="Confirm Password"/><br/><br/>
-
-              <RadioGroup name="user_type">
-                  <FormLabel htmlFor="user_type">Are you a:</FormLabel>
+                         label="Confirm Password"/><br/>
+              <FormLabel htmlFor="user_type">Are you a:</FormLabel>
+              <RadioGroup name="user_type" row={true}>
                   <FormControlLabel value="student" control={<Radio/>} id="user_student" label="Student"/>
                   <FormControlLabel value="tutor" control={<Radio/>} id="user_tutor" label="Tutor"/>
               </RadioGroup>
-
-              <Button id="cancel">Cancel</Button>
-              <Button id="register">Register</Button>
+              <Box row={true}>
+              <Button id="cancel" variant="contained" color="error">Cancel</Button>
+              <Button id="register" variant="contained">Register</Button>
+              </Box>
           </Box>
 
 
-      </div>
+      </Box>
   );
 }
 
