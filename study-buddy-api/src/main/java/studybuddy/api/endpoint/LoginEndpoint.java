d78d7ca574d3493ea77877dc26273bf5b847f9f0
Emma <e.aars@live.com>
2024-02-18 17:05:06 -0600

    attempting to connect Login backend to UI

--- study-buddy-api/src/main/java/studybuddy/api/endpoint/LoginEndpoint.java
+++ study-buddy-api/src/main/java/studybuddy/api/endpoint/LoginEndpoint.java
@@ -1,2 +1,33 @@
-package studybuddy.api.endpoint;public class LoginEndpoint {
+package studybuddy.api.endpoint;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import studybuddy.api.user.User;
+import studybuddy.api.user.UserRepository;
+
+@Log4j2
+@RestController
+//@RequestMapping("/api/users")
+public class LoginEndpoint {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //@PostMapping
+    @CrossOrigin(origins = "http://localhost:3000") // for local testing
+    //@CrossOrigin(origins = "http://34.16.169.60:3000")
+    @GetMapping("/login")
+    // ResponseEntity<User>
+    public String createUser(@RequestBody User registeredUser) {
+        if (registeredUser.getEmailAddress() == null || registeredUser.getPassword() == null) {
+            // Basic validation
+            //return ResponseEntity.badRequest().build();
+            return "womp womp";
+        }
+        // Ideally, encrypt the password here before saving
+        return registeredUser.getPassword();
+    }
 }
