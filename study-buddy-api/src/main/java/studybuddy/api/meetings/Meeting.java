9abc1d5191e1435fce57d56a1e1d98bb7a36f22f
cama0000 <acama0000@gmail.com>
2024-02-23 00:03:07 -0600

    added meetings table and new outline for viewMeetups page

---
+++ study-buddy-api/src/main/java/studybuddy/api/meetings/Meeting.java
@@ -0,0 +1,34 @@
+package studybuddy.api.meetings;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = Meeting.TABLE_NAME)
+public class Meeting {
+    public static final String TABLE_NAME = "MEETINGS";
+
+    @Id
+    @GeneratedValue(generator = TABLE_NAME + "_GENERATOR")
+    @SequenceGenerator(
+            name = TABLE_NAME + "_GENERATOR",
+            sequenceName = TABLE_NAME + "_SEQUENCE"
+    )
+    @Column(name = "MEETING_ID")
+    Long id;
+
+    @Column(name = "TITLE")
+    String title;
+
+    @Column(name = "DESCRIPTION")
+    String description;
+
+    @Column(name = "SUBJECT")
+    String subject;
+
+    @Column(name = "DATE")
+    LocalDateTime date;
+}
