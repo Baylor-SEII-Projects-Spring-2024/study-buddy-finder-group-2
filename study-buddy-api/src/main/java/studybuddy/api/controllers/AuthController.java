599d740726555854957dbca7e21b099481cea455
Emma <e.aars@live.com>
2024-02-27 00:44:46 -0600

    register now sets all user attributes

--- study-buddy-api/src/main/java/studybuddy/api/controllers/AuthController.java
+++ study-buddy-api/src/main/java/studybuddy/api/controllers/AuthController.java
@@ -8,10 +8,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import studybuddy.api.dto.AuthResponseDto;
 import studybuddy.api.dto.LoginDto;
@@ -21,6 +18,7 @@ import studybuddy.api.user.User;
 import studybuddy.api.user.UserService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") // for local testing
 @RequestMapping("/api/authorization")
 public class AuthController {
     private AuthenticationManager authenticationManager;
@@ -42,6 +40,7 @@ public class AuthController {
 
     @PostMapping("login")
     public ResponseEntity<AuthResponseDto> login(@RequestBody LoginDto loginDto) {
+        // TODO: how to get roles/userType in token (set authorities??)
         Authentication auth = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginDto.getUsername(),
                         loginDto.getPassword()));
@@ -53,14 +52,20 @@ public class AuthController {
 
     @PostMapping("register")
     public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
-        if(!userService.findByUsername(registerDto.getUsername()).isEmpty()) {
+        if(userService.findByUsername(registerDto.getUsername()).isPresent()) {
             return new ResponseEntity<>("Username is taken!", HttpStatus.BAD_REQUEST);
         }
 
+        // set all attributes for user
+        // make sure to give user userType and emailAddress!! (no underscores)
         User user = new User();
         user.setUsername(registerDto.getUsername());
         user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
-        // set all attributes
+        user.setUserType(registerDto.getUserType());
+        user.setName(registerDto.getName());
+        user.setEmailAddress(registerDto.getEmailAddress());
+        // TODO: add school to registerDto
+        // user.setSchool(registerDto.getSchool());
 
         userService.saveUser(user);
         return new ResponseEntity<>("User is registered!", HttpStatus.OK);
