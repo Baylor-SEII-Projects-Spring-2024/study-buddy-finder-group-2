91eaf680ea65e2c7b7c9095765744e133762455e
DanielleBoswell <danituongvy@gmail.com>
2024-03-10 15:11:29 -0500

    Adding to courses Endpoint

--- study-buddy-api/src/main/java/studybuddy/api/endpoint/AlterCoursesEnpoint.java
+++ study-buddy-api/src/main/java/studybuddy/api/endpoint/AlterCoursesEnpoint.java
@@ -1,10 +1,17 @@
 package studybuddy.api.endpoint;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+import studybuddy.api.course.CourseService;
 
 @Log4j2
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") // for local testing
+//@CrossOrigin(origins = "http://34.16.169.60:3000")
 public class AlterCoursesEnpoint {
+    @Autowired
+    CourseService courseService = new CourseService();
 
 }
