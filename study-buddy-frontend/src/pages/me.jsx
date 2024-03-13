ce48a85abf0203065ae465ee3c8927bccf6b318f
cama0000 <acama0000@gmail.com>
2024-03-12 23:17:50 -0500

    settings page in profiles

--- study-buddy-frontend/src/pages/me.jsx
+++ study-buddy-frontend/src/pages/me.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Card, CardContent, Stack, Typography, TextField} from '@mui/material';
+import { Button, Grid, Card, CardContent, Stack, Typography, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField} from '@mui/material';
 
 //This is the page that the user themself sees (able to edit and such)
 
-//TODO: Display username, name, courses, and a bio
+//TODO: Display username, name, courses, links, and a bio
+//TODO: Add a settings panel (dialog)
+//TODO: Use userID instead of username
 
 function MyInfoPage() {
-  const [editable, setEditable] = useState(false);
   const [text, setText] = useState('');
-  const [editSave, setEditSave] = useState();
+  const [realText, setRealText] = useState('');
   const [username, setUsername] = useState(null);
   const [user, setUser] = useState(null);
 
@@ -31,55 +32,106 @@ function MyInfoPage() {
         fetchUser(user);
     }, []);
 
-  const handleEdit = () =>{
-    setEditable(!editable);
+  const handleText = (event) =>{
+    //event.preventDefault();
+    //handleSettingsClose();
+    setText(event.target.value);
+    console.log("BIO TEXT: " + event.target.value);
   }
 
-  const handleText = (event) =>{
-    setText(event.target.value);
+  const handleSubmit = () =>{
+    event.preventDefault();
+    setRealText(text);
+
+    handleSettingsClose();
   }
+
+  //DIALOG (Settings)
+  const [settingsOpen, setSettingsOpen] = React.useState(false);
+
+  const handleSettingsOpen = () => {
+      setSettingsOpen(true);
+  };
+
+  const handleSettingsClose = () => {
+      setSettingsOpen(false);
+      setText(realText);
+  };
 
   return (
     <div>
-      <Stack sx={{ paddingTop: 4 }} alignItems='center' gap={2}>
+      <Stack sx={{ paddingTop: 4 , paddingBottom: 4}} alignItems='center' gap={2}>
         <Card sx={{ width: 300, margin: 'auto' }} elevation={4}>
           <CardContent>
             <Typography variant='h4' align='center'>My Profile</Typography>
           </CardContent>
         </ Card>
+      </Stack>
 
-        {user ? (
-        <Card sx={{ width: 1400, margin: 'auto' }} elevation={4}>
+      {user ? (
+        <Card sx={{ width: 1200, margin: 'auto', height: 400, marginBottom: '10px'}} elevation={4}>
           <CardContent>
             {/* Name and username */}
-            <strong style={{marginLeft: '100px'}}>{user.firstName} {user.lastName}</strong>
-            <div style={{marginLeft: '100px', color: 'gray'}}>@{user.username}</div>
+            <Grid container alignItems="center">
+              <Grid item sx={{ marginLeft: '100px', marginTop: '40px'}}>
+                <strong style={{fontSize:'20px'}}>{user.firstName} {user.lastName}</strong>
+                <div style={{ color: 'gray' }}>@{user.username}</div>
+              </Grid>
+
+              <Grid item sx={{ marginLeft: 'auto', marginRight: '100px', marginTop: '40px'}}>
+                <Button variant='contained' color="primary" onClick={handleSettingsOpen}>Edit Profile</Button>
+              </Grid>
+            </Grid>
             <br />
 
-            {/* BIO */}
-            {editable ? (
-              <div>
-                <TextField
-                  multiline
-                  rows={2}
-                  sx={{ width: '500px' }}
-                  style={{marginLeft: '100px'}}
-                  value={text}
-                  onChange={handleText}
-                />
-                <Button onClick={handleEdit}>Save</Button>
-              </div>
-            ) : (
-              <div>
-                <div style={{marginLeft: '100px'}}>{text}</div>
-                <Button style={{marginLeft: '100px'}} onClick={handleEdit}>Edit</Button>
-              </div>
-            )}
+            <Typography variant="body1" style={{ marginLeft: '100px'}}>
+              {realText}
+            </Typography>
           </CardContent>
-        </ Card>
+        </Card>
 
-           ) : null }
-      </Stack>
+      ) : null}
+
+
+
+
+
+
+      {/*CREATE SETTINGS DIALOG BOX*/}
+            <Dialog
+                open={settingsOpen}
+                onClose={handleSettingsClose}
+                component="form" validate="true" onSubmit={handleSubmit}
+            >
+
+        <DialogTitle>Profile Settings</DialogTitle>
+        <DialogContent>
+
+          <DialogContentText>
+            Edit your profile.
+          </DialogContentText>
+
+          <TextField
+            autoFocus
+            margin="dense"
+            id="bio"
+            name="bio"
+            label="Bio"
+            type="string"
+            fullWidth
+            variant="standard"
+            defaultValue={realText}
+            onChange={(e) => setText(e.target.value)}
+          />
+
+        </DialogContent>
+
+        <DialogActions>
+            <Button onClick={handleSettingsClose}>Cancel</Button>
+            <Button variant="contained" type="submit" onSubmit={handleSubmit} color="primary">Save</Button>
+        </DialogActions>
+        
+        </Dialog>
 
     </div>
   );
