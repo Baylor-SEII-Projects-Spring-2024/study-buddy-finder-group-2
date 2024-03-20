f9a23a93b5917350ef35d0b66595b81d65e3ccba
cama0000 <acama0000@gmail.com>
2024-03-20 14:35:18 -0500

    added profile files

---
+++ study-buddy-api/src/main/java/studybuddy/api/profile/Profile.java
@@ -0,0 +1,25 @@
+package studybuddy.api.profile;
+
+import jakarta.persistence.*;
+import lombok.Data;
+@Data
+@Entity
+@Table(name = Profile.TABLE_NAME)
+public class Profile { //ok
+    public static final String TABLE_NAME = "PROFILES";
+
+    @Id
+    @GeneratedValue(generator = TABLE_NAME + "_GENERATOR")
+    @SequenceGenerator(
+            name = TABLE_NAME + "_GENERATOR",
+            sequenceName = TABLE_NAME + "_SEQUENCE"
+    )
+    @Column(name = "PROFILE_ID")
+    Long id;
+
+    @Column(name = "USERNAME")
+    String username;
+
+    @Column(name = "BIO")
+    String bio;
+}
