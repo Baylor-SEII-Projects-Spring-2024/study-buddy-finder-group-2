84501b067f8c548c273da474fc927f903bb99f36
DanielleBoswell <danituongvy@gmail.com>
2024-03-23 14:49:37 -0500

    adding notification endpoint

--- study-buddy-api/src/main/java/studybuddy/api/endpoint/NotificationEndpoint.java
+++ study-buddy-api/src/main/java/studybuddy/api/endpoint/NotificationEndpoint.java
@@ -10,21 +10,26 @@ import java.util.HashMap;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
-@ServerEndpoint(value = "/api/notification/{username}")
-public class NotificationEndpoint {
-    private Session session;
-    private static Set<NotificationEndpoint> notificationEndpointSet = new CopyOnWriteArraySet<>();
-    private static HashMap<String, String> users = new HashMap<>();
-    @OnOpen
-    public void onOpen(Session session) throws IOException{
-        // Get session and WebSocket connection
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Configuration
+@EnableWebSocketMessageBroker
+//@CrossOrigin(origins = "http://localhost:3000") // for local testing
+@CrossOrigin(origins = "http://34.16.169.60:3000")
+public class NotificationEndpoint implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(final MessageBrokerRegistry registry){
+        registry.enableSimpleBroker("/api/notification");
     }
 
-    @OnClose
-    public void onClose(Session session) throws IOException {
-        // WebSocket connection closes
-
+    @Override
+    public void registerStompEndpoints(final StompEndpointRegistry registry){
+        registry.addEndpoint("/our-websocket");
     }
 
 }
