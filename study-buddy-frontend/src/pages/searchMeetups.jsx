6ee6b37437adac870297cffbee98ef6aef00806f
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2024-03-27 13:59:36 -0500

    update searchMeetups gui

--- study-buddy-frontend/src/pages/searchMeetups.jsx
+++ study-buddy-frontend/src/pages/searchMeetups.jsx
@@ -1,7 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import dayjs from 'dayjs';
-import { Box, Button, Card, CardContent, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, InputLabel, MenuItem, Select, Stack, TextField, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, Typography, TextField, FormControl, InputLabel, Select, MenuItem, ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: { main: '#a0c334' },
+        secondary: { main: '#ffd700' }
+    }
+});
 
 function SearchMeetupsPage() {
     const [searchStr, setStr] = useState(null);
@@ -15,6 +22,7 @@ function SearchMeetupsPage() {
     const [meetups, setMeetups] = useState([]);
     const [recommendedMeetups, setRecommendedMeetups] = useState([]);
     const [courses, setCourses] = useState([]);
+
 
     useEffect(() => {
         const params = new URLSearchParams(window.location.search);
@@ -32,31 +40,31 @@ function SearchMeetupsPage() {
     const handleSubmit = async (event) => {
         // prevents page reload
         event.preventDefault();
-    
+
         // get user time zone
         const options = await Intl.DateTimeFormat().resolvedOptions();
         const timezone = options.timeZone;
-    
+
         const meetup = {
             username, title, description, subject, date, location
         }
-    
+
         console.log("TITLEEEEEE: " + title);
         console.log("COURSEEEEE: " + subject);
-    
+
         // TODO: set error for empty search
         // axios.post("http://localhost:8080/api/searchMeetups", meetup, {
         //     headers: {
-        //     'timezone': timezone
-        // }}) // for local testing
+        //         'timezone': timezone
+        //     }}) // for local testing
         axios.post("http://34.16.169.60:8080/api/searchMeetups", meetup, {
-            headers: {
-            'timezone': timezone
-        }})
+               headers: {
+               'timezone': timezone
+            }})
             .then((res) => {
                 console.log(meetup.title)
                 console.log(meetup.subject)
-    
+
                 if(res.status === 200){
                     console.log(res.data[0])
                     setMeetups(res.data);
@@ -106,98 +114,86 @@ function SearchMeetupsPage() {
 
 
     return (
-        <div style={{ display: 'flex' }}>
-            {/* Recommended Meetups Section */}
-            <div style={{ width: '30%', padding: '10px' }}>
-                <Typography variant='h5' align='center'>Recommended Meetups</Typography>
-                {recommendedMeetups.map((meetup, index) => (
-                    <Card key={index} sx={{ marginBottom: 2 }} elevation={6}>
-                        <CardContent>
-                            <Typography><strong>Title:</strong> {meetup.title}</Typography>
-                            <Typography><strong>Description:</strong> {meetup.description}</Typography>
-                            {/* Add other details as needed */}
-                        </CardContent>
-                    </Card>
-                ))}
-            </div>
-
-            {/* Existing Search Meetups Section */}
-            <div style={{ width: '70%', padding: '10px' }}>
-                <Card sx={{ width: 520, margin: 'auto' }} elevation={4}>
-                    <CardContent>
-                        <Typography variant='h4' align='center'>Search Meetups</Typography>
-                    </CardContent>
-                </ Card>
-
-                {/* this is the search area, submits a form */}
-                <Box component="form" noValidate onSubmit={handleSubmit}
-                     sx={{ paddingTop: 3, width: 550, margin: 'auto' }}>
-                    <Stack spacing={4} direction="row" justifyContent="center">
-                        {/* search title */}
-                        <TextField
-                            required
-                            id="search"
-                            name="search"
-                            label="Title of Meeting"
-                            variant="outlined"
-                            onChange={(e) => handleSearch(e.target.value)}
-                        />
-
-                        {/* select course*/}
-                        <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
-                            <InputLabel required id="courseType">Course</InputLabel>
-                            <Select
-                                labelId="select course"
-                                id="select course"
-                                label="courseType"
-                                value={subject}
-                                onChange={(e) => setSubject(e.target.value)}
-                            >
-                                <MenuItem value=''><em>None</em></MenuItem>
-                                {courses.map(course => {
-                                    return (
-                                        <MenuItem key={course.courseId} value={course.coursePrefix + " " + course.courseNumber}>
-                                            {course.coursePrefix} {course.courseNumber}
-                                        </MenuItem>
-                                    )
-                                })}
-                            </Select>
-                        </FormControl>
-
-                        {/* submit the search form to get results */}
-                        <Button
-                            variant='contained'
-                            color="primary"
-                            type="submit"
-                        >
-                            Search</Button>
-                    </Stack>
+        <ThemeProvider theme={theme}>
+            <Box sx={{ display: 'flex', flexDirection: 'row', p: 2 }}>
+                <Box sx={{ width: '30%', marginRight: '2%' }}>
+                    <Typography variant="h5" sx={{ mb: 2, color: 'primary.main' }}>Recommended Meetups</Typography>
+                    {recommendedMeetups.map((meetup, index) => (
+                        <Card key={index} sx={{ mb: 2 }} elevation={6}>
+                            <CardContent>
+                                <Typography><strong>Title:</strong> {meetup.title}</Typography>
+                                <Typography><strong>Description:</strong> {meetup.description}</Typography>
+                            </CardContent>
+                        </Card>
+                    ))}
                 </Box>
 
-                {/* Display meetup results */}
-                {meetups.map((meetup, index) => (
-                    <Card key={index} sx={{ marginBottom: 2, width: 500}} elevation={6}>
+                <Box sx={{ width: '68%' }}>
+                    <Card sx={{ mb: 2 }} elevation={6}>
                         <CardContent>
-                            <Typography><strong>Title:</strong> {meetup.title}</Typography>
-                            <Typography><strong>Creator:</strong> {meetup.username}</Typography>
-                            <Typography><strong>Description:</strong> {meetup.description}</Typography>
-                            <Typography><strong>Course:</strong> {meetup.subject}</Typography>
-                            <Typography><strong>Date:</strong> {dayjs(meetup.date).format('MMMM DD, YYYY h:mm A')}</Typography>
-                            <Typography><strong>Location:</strong> {meetup.location}</Typography>
-                            <strong>Attendees:</strong>
-                            <ul style={{ listStyleType: 'none', paddingInlineStart: '20px' }}>
-                                {meetup.attendees.map((attendee, index) => (
-                                    <li key={index}>{'\u00A0\u00A0'}{attendee.username}</li>
-                                ))}
-                            </ul>
-                            <Button variant='contained' color="primary" size="small" onClick={() => handleJoin(meetup)}>
-                                Join Meetup
-                            </Button>
+                            <Typography variant="h4" sx={{ color: 'primary.main' }}>Search Meetups</Typography>
+                            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                                <TextField
+                                    fullWidth
+                                    id="search"
+                                    label="Title of Meeting"
+                                    variant="outlined"
+                                    value={searchStr}
+                                    onChange={(e) => handleSearch(e.target.value)}
+                                    sx={{ mb: 2 }}
+                                />
+                                <FormControl fullWidth variant="filled" sx={{ mb: 2 }}>
+                                    <InputLabel id="courseType">Course</InputLabel>
+                                    <Select
+                                        labelId="select-course"
+                                        id="select-course"
+                                        value={subject}
+                                        onChange={(e) => setSubject(e.target.value)}
+                                    >
+                                        <MenuItem value=""><em>None</em></MenuItem>
+                                        {courses.map((course) => (
+                                            <MenuItem key={course.courseId} value={course.coursePrefix + " " + course.courseNumber}>
+                                                {course.coursePrefix} {course.courseNumber}
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    type="submit"
+                                    sx={{ display: 'block', margin: '0 auto' }}
+                                >
+                                    Search
+                                </Button>
+                            </Box>
                         </CardContent>
                     </Card>
-                ))}
-            </div>
-        </div>
+
+                    {meetups.map((meetup, index) => (
+                        <Card key={index} sx={{ margin: 2, width: '80%' }} elevation={6}>
+                            <CardContent>
+                                <Typography><strong>Title:</strong> {meetup.title}</Typography>
+                                <Typography><strong>Creator:</strong> {meetup.username}</Typography>
+                                <Typography><strong>Description:</strong> {meetup.description}</Typography>
+                                <Typography><strong>Course:</strong> {meetup.subject}</Typography>
+                                <Typography><strong>Date:</strong> {dayjs(meetup.date).format('MMMM DD, YYYY h:mm A')}</Typography>
+                                <Typography><strong>Location:</strong> {meetup.location}</Typography>
+                                <strong>Attendees:</strong>
+                                <ul style={{ listStyleType: 'none', paddingInlineStart: '20px' }}>
+                                    {meetup.attendees.map((attendee, index) => (
+                                        <li key={index}>{'\u00A0\u00A0'}{attendee.username}</li>
+                                    ))}
+                                </ul>
+                                <Button variant='contained' color="primary" size="small" onClick={() => handleJoin(meetup)}>
+                                    Join Meetup
+                                </Button>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </Box>
+            </Box>
+        </ThemeProvider>
     );
 }
 
