8252ef2a494327ca0b574bb663eaf46050ec2274
DanielleBoswell <danituongvy@gmail.com>
2024-04-01 16:22:50 -0500

    setting up notifications

--- study-buddy-api/src/main/java/studybuddy/api/notifications/NotificationRepository.java
+++ study-buddy-api/src/main/java/studybuddy/api/notifications/NotificationRepository.java
@@ -1,2 +1,14 @@
-package studybuddy.api.notifications;public class NotificationRepository {
+package studybuddy.api.notifications;
+
+import org.aspectj.weaver.ast.Not;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    public List<Notification> findNotificationsByReciever_IdOrderByTimestampDesc(Long reciever_id);
+
+    public List<Notification> findNotificationsByReciever_UsernameOrderByTimestampDesc(String reciever_username);
 }
--- study-buddy-api/src/main/java/studybuddy/api/notifications/NotificationService.java
+++ study-buddy-api/src/main/java/studybuddy/api/notifications/NotificationService.java
@@ -1,2 +1,55 @@
-package studybuddy.api.notifications;public class NotificationService {
+package studybuddy.api.notifications;
+
+import org.aspectj.weaver.ast.Not;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NotificationService {
+    @Autowired
+    NotificationRepository notificationRepository;
+
+    /**
+     * getNotificationsByRecieverId
+     *
+     *
+     * @param reciever_id
+     * @return
+     */
+    public List<Notification> getNotificationsByRecieverId(Long reciever_id){
+        return notificationRepository
+                .findNotificationsByReciever_IdOrderByTimestampDesc(reciever_id);
+    }
+
+    /**
+     * getNotificationsByRecieverUsername
+     *
+     * @param username
+     * @return
+     */
+    public List<Notification> getNotificationsByRecieverUsername(String username){
+        return notificationRepository
+                .findNotificationsByReciever_UsernameOrderByTimestampDesc(username);
+    }
+
+
+    /**
+     * sendNotification
+     *
+     * @param notification
+     * @return
+     */
+    public Notification sendNotification(Notification notification){
+        notification.setRead(false);
+        notificationRepository.save(notification);
+        return notification;
+    }
+
+    public Notification changeReadStatus(Notification notification){
+        notification.setRead(!notification.isRead());
+        return notificationRepository.save(notification);
+    }
+
 }
