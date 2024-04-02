9f632747afc3ac6080165a95bc6352e8b3910ce5
Emma <e.aars@live.com>
2024-04-02 14:38:25 -0500

    fixing searchUsers (I left out the recommendations)

--- study-buddy-frontend/src/pages/searchUsers.jsx
+++ study-buddy-frontend/src/pages/searchUsers.jsx
@@ -26,10 +26,9 @@ function SearchUsersPage() {
     const [emailAddress, setEmail] = useState(null);
     const [userType, setType] = useState(null);
     const [school, setSchool] = useState(null);
-
     const [searchStr, setStr] = useState(null);
-
     const [users, setUsers] = useState([]);
+    const [recommendedUsers, setRecommendedUsers] = useState([]);
     const [selectedUser, setSelectedUser] = useState(null);
 
     const [id, setId] = useState(null);
@@ -38,10 +37,25 @@ function SearchUsersPage() {
     const [isConnected, setIsConnected] = useState(null);
     const [selectedConnection, setSelectedConnection] = useState(null);
 
+    const fetchRecommendations = (username) => {
+        //axios.get(`http://localhost:8080/api/recommendations/${username}`) // Adjust as needed
+        axios.get(`http://34.16.169.60:8080/api/recommendations/${username}`)
+            .then((response) => {
+                setRecommendedUsers(response.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching recommendations:', error);
+            });
+    };
+
     // get the user's username
     useEffect(() => {
         const params = new URLSearchParams(window.location.search),
             user = params.get("username");
+
+        if (user) {
+            fetchRecommendations(user);
+        }
 
         setRequester(user);
     }, [])
@@ -181,138 +195,151 @@ function SearchUsersPage() {
     };
 
     return (
-        <div>
-            <Stack sx={{ paddingTop: 4 }} alignItems='center' gap={2}>
-                <Card sx={{ width: 520, margin: 'auto' }} elevation={4}>
-                    <CardContent>
-                        <Typography variant='h4' align='center'>Search Users</Typography>
-                    </CardContent>
-                </ Card>
-
-                {/* this is the search area, submits a form */}
-                <Box component="form" noValidate onSubmit={handleSubmit}
-                     sx={{ paddingTop: 3, width: 550, margin: 'auto' }}>
-                    <Stack spacing={4} direction="row" justifyContent="center">
-                        {/* get search string for name and username */}
-                        <TextField
-                            required
-                            id="search"
-                            name="search"
-                            label="Name or Username"
-                            variant="outlined"
-                            onChange={(e) => handleSearch(e.target.value)}
-                        />
-
-                        {/* get requested user type (none, student, tutor) */}
-                        <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
-                            <InputLabel required id="userType">User Type</InputLabel>
-                            <Select
-                                labelId="select userType"
-                                id="select userType"
-                                label="userType"
-                                onChange={(e) => setType(e.target.value)}
-                            >
-                                <MenuItem value={null}>
-                                    <em>None</em>
-                                </MenuItem>
-                                <MenuItem value={"student"}>Student</MenuItem>
-                                <MenuItem value={"tutor"}>Tutor</MenuItem>
-                            </Select>
-                        </FormControl>
-
-                        {/* submit the search form to get results */}
-                        <Button
-                            variant='contained'
-                            color="primary"
-                            type="submit"
-                        >
-                            Search</Button>
-                    </Stack>
-                </Box>
-
-                {/* display all matches on separate cards */}
-                {users.map((user, index) => (
-                    <Card key={index}
-                          sx={{ width: 520, margin: 'auto', marginTop: 1, cursor: 'pointer' }}
-                          elevation={6}>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+            <Box sx={{width: '30%'}}>
+                <Typography variant='h6'>Recommended Users</Typography>
+                {recommendedUsers.map((user, index) => (
+                    <Card key={index} sx={{marginBottom: 2}}>
                         <CardContent>
-                            <Box sx={{ paddingTop: 3, width: 400, margin: 'auto' }}>
-                                <Stack spacing={13} direction="row" justifyContent="space-evenly">
-                                    <Box sx={{ width: 200 }}>
-                                        <ul style={{ listStyleType: 'none', padding: 0, margin: 0}}>
-                                            <li>
-                                                <strong>Username: </strong> {user.username}
-                                                <br />
-                                                <strong>Name: </strong> {user.firstName + " " + user.lastName}
-                                                <br />
-                                            </li>
-                                        </ul>
-                                    </Box>
-
-                                    {/* view the profile of that user */}
-                                    <Button
-                                        variant='contained'
-                                        color= "primary"
-                                        size="small"
-                                        onClick={() => handleClickOpenProfile(user)}
-                                    >
-                                        View Profile</Button>
-                                </Stack>
-                            </Box>
+                            <Typography variant='subtitle1'>{user.username}</Typography>
+                            {/* Other user details here */}
                         </CardContent>
                     </Card>
                 ))}
+            </Box>
+            <Box sx={{width: '70%'}}>
+                <Stack sx={{ paddingTop: 4 }} alignItems='center' gap={2}>
+                    <Card sx={{ width: 520, margin: 'auto' }} elevation={4}>
+                        <CardContent>
+                            <Typography variant='h4' align='center'>Search Users</Typography>
+                        </CardContent>
+                    </ Card>
 
-                {/* add button back to user's landing page */}
-                {/*<Button
+                    {/* this is the search area, submits a form */}
+                    <Box component="form" noValidate onSubmit={handleSubmit}
+                         sx={{ paddingTop: 3, width: 550, margin: 'auto' }}>
+                        <Stack spacing={4} direction="row" justifyContent="center">
+                            {/* get search string for name and username */}
+                            <TextField
+                                required
+                                id="search"
+                                name="search"
+                                label="Name or Username"
+                                variant="outlined"
+                                onChange={(e) => handleSearch(e.target.value)}
+                            />
+
+                            {/* get requested user type (none, student, tutor) */}
+                            <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+                                <InputLabel required id="userType">User Type</InputLabel>
+                                <Select
+                                    labelId="select userType"
+                                    id="select userType"
+                                    label="userType"
+                                    onChange={(e) => setType(e.target.value)}
+                                >
+                                    <MenuItem value={null}>
+                                        <em>None</em>
+                                    </MenuItem>
+                                    <MenuItem value={"student"}>Student</MenuItem>
+                                    <MenuItem value={"tutor"}>Tutor</MenuItem>
+                                </Select>
+                            </FormControl>
+
+                            {/* submit the search form to get results */}
+                            <Button
+                                variant='contained'
+                                color="primary"
+                                type="submit"
+                            >
+                                Search</Button>
+                        </Stack>
+                    </Box>
+
+                    {/* display all matches on separate cards */}
+                    {users.map((user, index) => (
+                        <Card key={index}
+                              sx={{ width: 520, margin: 'auto', marginTop: 1, cursor: 'pointer' }}
+                              elevation={6}>
+                            <CardContent>
+                                <Box sx={{ paddingTop: 3, width: 400, margin: 'auto' }}>
+                                    <Stack spacing={13} direction="row" justifyContent="space-evenly">
+                                        <Box sx={{ width: 200 }}>
+                                            <ul style={{ listStyleType: 'none', padding: 0, margin: 0}}>
+                                                <li>
+                                                    <strong>Username: </strong> {user.username}
+                                                    <br />
+                                                    <strong>Name: </strong> {user.firstName + " " + user.lastName}
+                                                    <br />
+                                                </li>
+                                            </ul>
+                                        </Box>
+
+                                        {/* view the profile of that user */}
+                                        <Button
+                                            variant='contained'
+                                            color= "primary"
+                                            size="small"
+                                            onClick={() => handleClickOpenProfile(user)}
+                                        >
+                                            View Profile</Button>
+                                    </Stack>
+                                </Box>
+                            </CardContent>
+                        </Card>
+                    ))}
+
+                    {/* add button back to user's landing page */}
+                    {/*<Button
                     variant="outlined"
                     color="error"
                     href="/"
                 >
                     Back</Button>*/}
 
-            </Stack>
+                </Stack>
 
-            {/*View user profile and add as connection*/}
-            <Dialog
-                open={openProfile}
-                onClose={handleCloseProfile}
-                fullWidth
-                component="form"
-                validate="true"
-                onSubmit={handleConnection}
-            >
-                <DialogTitle variant='s2'>{firstName + " " + lastName}'s Profile</DialogTitle>
-                <DialogContent>
-                    <Stack spacing={2}>
-                        <Typography variant='s2'>{userType}</Typography>
-                        <Typography variant='s2'></Typography>
-                        <Typography variant='s1'>Username: {username}</Typography>
-                        <Typography variant='s1'>Email: {emailAddress}</Typography>
-                        <Typography variant='s1'>School: {school}</Typography>
-                        <Typography variant='s1'>Courses...</Typography>
-                    </Stack>
-                </DialogContent>
+                {/*View user profile and add as connection*/}
+                <Dialog
+                    open={openProfile}
+                    onClose={handleCloseProfile}
+                    fullWidth
+                    component="form"
+                    validate="true"
+                    onSubmit={handleConnection}
+                >
+                    <DialogTitle variant='s2'>{firstName + " " + lastName}'s Profile</DialogTitle>
+                    <DialogContent>
+                        <Stack spacing={2}>
+                            <Typography variant='s2'>{userType}</Typography>
+                            <Typography variant='s2'></Typography>
+                            <Typography variant='s1'>Username: {username}</Typography>
+                            <Typography variant='s1'>Email: {emailAddress}</Typography>
+                            <Typography variant='s1'>School: {school}</Typography>
+                            <Typography variant='s1'>Courses...</Typography>
+                        </Stack>
+                    </DialogContent>
 
-                <DialogActions>
-                    <Button
-                        variant="outlined"
-                        color="error"
-                        onClick={handleCloseProfile}
-                    >
-                        Cancel</Button>
-                    <Button
-                        id="connection"
-                        variant="contained"
-                        sx={{
-                            backgroundColor: isConnected ? 'purple' : 'light blue',
-                        }}
-                        type="submit"
-                        onClick={handleSetConnection}
-                    >
-                        {text}</Button>
-                </DialogActions>
-            </Dialog>
+                    <DialogActions>
+                        <Button
+                            variant="outlined"
+                            color="error"
+                            onClick={handleCloseProfile}
+                        >
+                            Cancel</Button>
+                        <Button
+                            id="connection"
+                            variant="contained"
+                            sx={{
+                                backgroundColor: isConnected ? 'purple' : 'light blue',
+                            }}
+                            type="submit"
+                            onClick={handleSetConnection}
+                        >
+                            {text}</Button>
+                    </DialogActions>
+                </Dialog>
+            </Box>
         </div>
     );
 }
