049d6b3f0bd5e65fadace9b2b2c27542113635b7
cama0000 <acama0000@gmail.com>
2024-04-20 11:34:43 -0500

    fix endpoint

--- study-buddy-frontend/src/pages/Notification.jsx
+++ study-buddy-frontend/src/pages/Notification.jsx
@@ -46,8 +46,8 @@ function NotificationPage() {
 
 
     const api = axios.create({
-        baseURL: 'http://localhost:8080/'
-        //baseURL: 'http://34.16.169.60:8080/'
+        //baseURL: 'http://localhost:8080/'
+        baseURL: 'http://34.16.169.60:8080/'
     });
 
     const checkExpiredMeetups = async (user) => {
