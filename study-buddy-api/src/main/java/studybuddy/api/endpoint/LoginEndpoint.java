dc4127c67d2c4465c81f8ebc1f9e816e95494666
DanielleBoswell <danituongvy@gmail.com>
2024-04-24 17:11:56 -0500

    altering endpoints

--- study-buddy-api/src/main/java/studybuddy/api/endpoint/LoginEndpoint.java
+++ study-buddy-api/src/main/java/studybuddy/api/endpoint/LoginEndpoint.java
@@ -54,8 +54,8 @@ import studybuddy.api.user.UserService;
 
 @Log4j2
 @RestController
-@CrossOrigin(origins = "http://localhost:3000") // for local testing
-//@CrossOrigin(origins = "http://34.16.169.60:3000")
+//@CrossOrigin(origins = "http://localhost:3000") // for local testing
+@CrossOrigin(origins = "http://34.16.169.60:3000")
 public class LoginEndpoint {
 
     @Autowired
