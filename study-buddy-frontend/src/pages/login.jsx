5d0caa817660c23548dcf3c2e75d3e0982228a75
Emma <e.aars@live.com>
2024-04-28 17:49:14 -0500

    missed login.jsx cross origin

--- study-buddy-frontend/src/pages/login.jsx
+++ study-buddy-frontend/src/pages/login.jsx
@@ -52,8 +52,8 @@ function LoginPage() {
     const dispatch = useDispatch(); // use to change state
 
     const api = axios.create({
-        baseURL: 'http://localhost:8080/',
-        //baseURL: 'http://34.16.169.60:8080/',
+        //baseURL: 'http://localhost:8080/',
+        baseURL: 'http://34.16.169.60:8080/',
     });
 
 
