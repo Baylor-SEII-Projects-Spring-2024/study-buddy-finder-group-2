6fb12369d2d29fd68f66914a79b6aa8b527fdb01
Winecrow <143528286+Winecrow@users.noreply.github.com>
2024-04-28 16:04:12 -0500

    Update makeRatings.jsx

--- study-buddy-frontend/src/pages/makeRatings.jsx
+++ study-buddy-frontend/src/pages/makeRatings.jsx
@@ -57,6 +57,7 @@ function RatingsPage() {
 
   const fetchPendingRatings = async (user) => {
     try {
+        console.log(user);
         const res = await api.get(`newRatings/${user}`);
         console.log(res);
         setRatings(res.data);
