74d65fb813b1818e7877c397fbb74355fd87ab91
DanielleBoswell <danituongvy@gmail.com>
2024-04-29 12:54:40 -0500

    changing meetups to search

--- study-buddy-frontend/src/pages/searchMeetups.jsx
+++ study-buddy-frontend/src/pages/searchMeetups.jsx
@@ -1,7 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import dayjs from 'dayjs';
-import { Box, Button, Card, CardContent, Typography, TextField, FormControl, InputLabel, Select, MenuItem, ThemeProvider, createTheme } from "@mui/material";
+import {
+    Box,
+    Button,
+    Card,
+    CardContent,
+    Typography,
+    TextField,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+    ThemeProvider,
+    createTheme,
+    Tooltip
+} from "@mui/material";
 import NotificationPage from "@/pages/Notification";
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import PersonIcon from '@mui/icons-material/Person';
@@ -13,6 +27,7 @@ import {useDispatch, useSelector} from "react-redux";
 import Avatar from '@mui/material/Avatar';
 import Head from "next/head";
 import {deauthorize} from "@/utils/authSlice";
+import NotesIcon from "@mui/icons-material/Notes";
 
 const theme = createTheme({
     palette: {
@@ -254,78 +269,103 @@ function SearchMeetupsPage() {
                 <Box sx={{ width: '30%', marginRight: '2%' }}>
                     <Typography variant="h5" sx={{ mb: 2, color: 'primary.main' }}>Recommended Meetups</Typography>
                     {recommendedMeetups.filter(r => new Date(r.startDate) > new Date()).map((meetup, index) => (
-                        <Card key={index} sx={{ mb: 2 }} elevation={6}>
+                        <Card key={index} sx={{
+                            margin: 2,
+                            width: 350,
+                            height: 500,
+                            display:"flex",
+                            justifyContent:"space-around",
+                            alignItems:"center",
+                            borderRadius: '16px', // Rounded corners
+                            boxShadow: '0 4px 8px rgba(0,0,0,0.1)', // Subtle shadow
+                            backgroundColor: '#f0f0f0', // Light gray background for modern look
+                            transition: 'box-shadow 0.3s', // Smooth shadow transition
+                            '&:hover': {
+                                boxShadow: '0 6px 12px rgba(0,0,0,0.2)' // Enhanced shadow on hover
+                            }
+                        }}>
                             <CardContent>
-                            <Typography variant='h4' align='center' sx={{ marginTop: '20px', fontWeight: 'bold', fontSize: '20px' }}>{meetup.title}</Typography>
+                                <Typography variant='h5' align='center' sx={{
+                                    color: '#2e7d32',
+                                    fontWeight: 'bold',
+                                    marginTop: 2
+                                }}>{meetup.title}</Typography>
 
-                            <div style={{ display: 'flex', alignItems: 'center', marginTop: '5px', marginLeft: '5px'}}>
-                                <PersonIcon sx={{ fontSize: '20px', marginRight: '5px' }} />
-                                <span style={{ color: 'gray', fontStyle: 'italic', marginRight: '20px', fontSize: '14px' }}>@{meetup.username}</span>
-                            </div>
+                                <Tooltip title={"creator"}><PersonIcon sx={{fontSize: 20, mr: 1, color: 'primary.main'}}/></Tooltip>
+                                <span style={{
+                                    color: 'gray',
+                                    fontStyle: 'italic',
+                                }}>@{meetup.username}</span>
+                                <Typography
+                                    sx={{mt: 1, color: '#666', fontSize: 14}}>
+                                    <Tooltip title={"description"}>
+                                        <NotesIcon sx={{fontSize: 20, mr: 1, color: 'primary.main'}}/></Tooltip>
+                                    {meetup.description}</Typography>
+                                <Typography
+                                    sx={{mt: 1, color: '#666', fontSize: 14}}>
+                                    <Tooltip title={"subject"}>
+                                        <CreateIcon sx={{fontSize: 20, mr: 1, color: 'primary.main'}} /> </Tooltip>
+                                    {meetup.subject}</Typography>
 
-                            <br />
+                                <Typography sx={{mt: 2, display: 'flex', alignItems: 'center', color: '#333'}}>
+                                    <Tooltip title={"when"}>
+                                        <EventIcon sx={{fontSize: 20, mr: 1, color: 'primary.main'}}/></Tooltip>
+                                    {dayjs(meetup.startDate).format('MMMM DD, YYYY h:mm A')} - {dayjs(meetup.endDate).format('MMMM DD, YYYY h:mm A')}
+                                </Typography>
 
-                            <span style={{ marginLeft: '20px', fontSize: '14px' }}>{meetup.description}</span>
+                                <Typography sx={{display: 'flex', alignItems: 'center', mt: 1, color: '#333'}}>
+                                    <Tooltip title={"where"}>
+                                        <LocationOnIcon sx={{fontSize: 20, mr: 1, color: 'primary.main'}}/></Tooltip>
+                                    {meetup.location}
+                                </Typography>
 
-                            <div style={{ display: 'flex', alignItems: 'center', marginTop: '5px', marginLeft: '5px'}}>
-                                <CreateIcon sx={{ fontSize: '20px', marginRight: '5px' }} />
-                                <span style={{ fontSize: '14px' }}>{meetup.subject}</span>
-                            </div>
+                                <Typography variant='subtitle1'
+                                            sx={{mt: 2, fontWeight: 'bold'}}>Attendees</Typography>
+                                <Typography variant='subtitle2'
+                                            sx={{mt:1, fontWeight: 'bold'}}>Students</Typography>
+                                <ul sx={{listStyleType: 'none', padding: 0}}>
+                                    {meetup.attendees.filter(attendee => attendee.userType === 'student').map((attendee, index) => (
+                                        <li key={index} sx={{color: '#777', fontSize: 14, mt: 1}}>
+                                            <Avatar sx={{width: 20, height: 20, mr: 1}} src={attendee.pictureUrl}/>
+                                            {attendee.username}
+                                        </li>
+                                    ))}
+                                </ul>
 
-                            <div style={{ display: 'flex', alignItems: 'center', marginTop: '5px', marginLeft: '5px'}}>
-                                <EventIcon sx={{ fontSize: '20px', marginRight: '5px' }} />
-                                <span style={{ fontSize: '14px' }}>{dayjs(meetup.startDate).format('MMMM DD, YYYY h:mm A')} - {dayjs(meetup.endDate).format('MMMM DD, YYYY h:mm A')}</span>
-                            </div>
-
-                            <div style={{ display: 'flex', alignItems: 'center', marginTop: '5px', marginLeft: '5px'}}>
-                                <LocationOnIcon sx={{ fontSize: '20px', marginRight: '5px' }} />
-                                <span style={{ fontSize: '14px' }}>{meetup.location}</span>
-                            </div>
-
-                            <br />
-
-                            <Typography variant='h4' sx={{ fontSize: '15px', fontWeight: 'bold', marginLeft: '5px'}}>Attendees</Typography>
-
-                            {/* map students */}
-                            <ul style={{ listStyleType: 'none', paddingInlineStart: '30px' }}>
-                                <Typography variant='h6' sx={{ fontSize: '13px', fontWeight: 'bold', marginLeft: '10px' }}>Students</Typography>
-                                {meetup.attendees.filter(attendee => attendee.userType === 'student').map((attendee, index) => (
-                                    <li key={index} style={{ color: 'gray', fontStyle: 'italic', marginRight: '20px', display: 'flex', alignItems: 'center' }}>
-                                        <Avatar sx={{ width: 20, height: 20, marginRight: '5px' }} src={attendee.pictureUrl} />
-                                        <span>{attendee.username}</span>
-                                    </li>
-                                ))}
-                            </ul>
-
-                            {/* map tutors */}
-                            <ul style={{ listStyleType: 'none', paddingInlineStart: '30px' }}>
-                                <Typography variant='h6' sx={{ fontSize: '13px', fontWeight: 'bold', marginLeft: '10px', marginTop: '5px'}}>Tutors</Typography>
-                                {meetup.attendees.filter(attendee => attendee.userType === 'tutor').map((attendee, index) => (
-                                    <li key={index} style={{ color: 'gray', fontStyle: 'italic', marginRight: '20px', display: 'flex', alignItems: 'center' }}>
-                                        <Avatar sx={{ width: 20, height: 20, marginRight: '5px' }} src={attendee.pictureUrl} />
-                                        {username !== attendee.username && ( // Compare the usernames
-                                            <span onClick={() => handleUsernameClick(attendee.username)} style={{ textDecoration: 'underline', color: 'blue', cursor: 'pointer' }}>
-                                                {attendee.username}
-                                            </span>
-                                        )}
-                                        {username === attendee.username && (
-                                            <span>{attendee.username}</span> // Render differently if the usernames are equal
-                                        )}
-                                    </li>
-                                ))}
-                            </ul>
-
-                            {meetup.attendees.some(attendee => attendee.username === currentUser) ? (
-                            <Button variant='contained' size="small" style={{ backgroundColor: 'red', color: 'white', marginTop: '10px'}} onClick={() => handleLeave(meetup)}>
-                                Leave Meetup
-                            </Button>
-                            ) : (
-                                <Button variant='contained' color="primary" size="small" style={{ marginTop: '10px' }} onClick={() => handleJoin(meetup)}>
-                                    Join Meetup
-                                </Button>
-                            )}
+                                <Typography variant='subtitle2' sx={{mt: 1, fontWeight: 'bold'}}>Tutors</Typography>
+                                <ul sx={{listStyleType: 'none', padding: 0}}>
+                                    {meetup.attendees.filter(attendee => attendee.userType === 'tutor').map((attendee, index) => (
+                                        <li key={index} sx={{color: '#777', fontSize: 14, mt: 1}}>
+                                            <Avatar sx={{width: 20, height: 20, mr: 1}} src={attendee.pictureUrl}/>
+                                            {attendee.username}
+                                        </li>
+                                    ))}
+                                </ul>
 
 
+                                {meetup.attendees.some(attendee => attendee.username === username) ? (
+                                    <Button variant='contained' color="error" sx={{
+                                        mt: 2,
+                                        width: '100%',
+                                    }}
+                                            onClick={() => handleLeave(meetup)}
+                                    >
+                                        Leave Meetup
+                                    </Button>
+                                ) : (
+                                    <Button variant='contained' sx={{
+                                        mt: 2,
+                                        width: '100%',
+                                        backgroundColor: '#2e7d32', // Dark green color for the button
+                                        '&:hover': {
+                                            backgroundColor: '#1e4d27' // Darker shade of green on hover
+                                        }
+                                    }}
+                                            onClick={() => handleJoin(meetup)}
+                                    >
+                                        Join Meetup
+                                    </Button>
+                                )}
                             </CardContent>
                         </Card>
                     ))}
@@ -400,7 +440,7 @@ function SearchMeetupsPage() {
                         
                             <div style={{ display: 'flex', alignItems: 'center', marginTop: '5px', marginLeft: '10px'}}>
                                 <PersonIcon sx={{ fontSize: '25px', marginRight: '5px' }} />
-                                <span style={{ color: 'gray', fontStyle: 'italic', marginRight: '30px' }}>@{meetup.username}</span>
+                                <span style={{ color: 'gray', fontStyle: 'italic', marginRight: '30px' }}>Owner: @{meetup.username}</span>
                             </div>
 
                             <br />
